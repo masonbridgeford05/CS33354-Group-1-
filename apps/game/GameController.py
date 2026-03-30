9dfbb02a0194b36a929ea6624acc64fe44585b49
Chi-Heng Chan <seanchan2006@gmail.com>
2026-03-30 11:02:26 -0500

    Added python environment and GameController

---
+++ apps/game/GameController.py
@@ -0,0 +1,15 @@
+from PIL import Images
+
+class GameController:
+
+
+    def __innit__(self, gamemode):
+        self.gamemode
+        # create game id
+        # create game object
+        # initialize gamemode
+
+
+    
+
+    
