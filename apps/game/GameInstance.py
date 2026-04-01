6d14e477c800fe5aae3942ffd6e8d57688f985e4
Chi-Heng Chan <seanchan2006@gmail.com>
2026-04-01 11:11:30 -0500

    Created test case for image creation.

--- apps/game/GameInstance.py
+++ apps/game/GameInstance.py
@@ -1,14 +1,11 @@
 from PIL import Image
 
-class Game:
+class GameInstance:
     
     def __init__(self, gamemode):
         self.score = 0
         self.gamemode = gamemode
         self.attempts = 0
     
-    def startGame(self, gamemode):
-        
-
-    
-
+    def startGame(self, gamemode, image_path, answer):
+        self
--- apps/game/models.py
+++ apps/game/models.py
@@ -7,12 +7,15 @@ class GameImage(models.Model):
     location = models.CharField( max_length=50)
     image_path = models.ImageField(upload_to='GameImages/')
 
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)  # saves to database first
-        img = Image.open(self.image.path)  # opens the uploaded image
-        if img.size != (1280, 720):        # checks if wrong size
-            img = img.resize((1280, 720))  # resizes it
-            img.save(self.image.path)      # overwrites with resized version
+    def save(self, *args, **kwargs): # ← Django calls this automatically
+        super().save(*args, **kwargs)  
+        try:
+            img = Image.open(self.image_path.path)
+            if img.size != (1280, 720):
+                img = img.resize((1280, 720))
+                img.save(self.image_path.path)
+        except (FileNotFoundError, ValueError):
+            pass
 
     def __str__(self):
         return f"{self.location} - {self.difficulty}"
