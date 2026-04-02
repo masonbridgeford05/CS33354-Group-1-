8b438137399883bd9b3df011642123307bf9c5f1
muskaanc7 <muskaan.chilakala@gmail.com>
2026-04-01 20:22:26 -0500

    Add models, validator, and views to accounts app

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.hashers import make_password, check_password
 
 # Create your models here.
+
+#DBMgr
+class User(models.Model):
+    userId = models.AutoField(primary_key = True)
+    userName = models.CharField(max_length = 32)
+    userEmail = models.EmailField(unique = True)
+    userPassword = models.CharField(max_length = 64)
+
+    def createUserAccount(userName, userEmail, userPassword):
+        hashed_password = make_password(userPassword)
+        user = User(userName = userName, userEmail = userEmail, userPassword = hashed_password)
+        user.save()
+        return user
+
+    def checkUserCredentials(userEmail, userPassword):
+        try:
+            user = User.objects.get(userEmail = userEmail)
+            return check_password(userPassword, user.user_password)
+        except User.DoesNotExist:
+            return False
