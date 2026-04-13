3635d650549654d08d8bbaa82af90eb4de668b72
Chi-Heng Chan <seanchan2006@gmail.com>
2026-04-13 10:23:27 -0500

    Fixing merge result

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, redirect
-from django.shortcuts import redirect
 from django.http import HttpResponse
 from django.views import View
 from django.contrib.auth import authenticate, login, logout
